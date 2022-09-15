eb0f85cfcb9bca731c74de336e85f83f381cc735
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-15 11:27:43 +0200

    Create Livre entity #2

---
+++ src/Entity/Livre.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LivreRepository::class)
+ */
+class Livre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=5000)
+     */
+    private $summary;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $number_of_page;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_parution;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getSummary(): ?string
+    {
+        return $this->summary;
+    }
+
+    public function setSummary(string $summary): self
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    public function getNumberOfPage(): ?int
+    {
+        return $this->number_of_page;
+    }
+
+    public function setNumberOfPage(int $number_of_page): self
+    {
+        $this->number_of_page = $number_of_page;
+
+        return $this;
+    }
+
+    public function getDateParution(): ?\DateTimeInterface
+    {
+        return $this->date_parution;
+    }
+
+    public function setDateParution(\DateTimeInterface $date_parution): self
+    {
+        $this->date_parution = $date_parution;
+
+        return $this;
+    }
+}
