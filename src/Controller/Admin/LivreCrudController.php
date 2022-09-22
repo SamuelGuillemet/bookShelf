7757d01f259c5143e2c992839dbb15f42c2c29c0
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-22 21:33:25 +0200

    Add the CRUD controller for Livre #4

---
+++ src/Controller/Admin/LivreCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Livre;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+
+class LivreCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Livre::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('titre'),
+            TextEditorField::new('summary'),
+            NumberField::new('number_of_page'),
+            DateField::new('date_parution'),
+            AssociationField::new('bibliotheque'),
+        ];
+    }
+}
