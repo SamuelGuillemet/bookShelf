795e46b14765989ab1d4d20c0ace0db81b00788b
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-22 22:56:24 +0200

    Modify the data in the base #3

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,55 +6,68 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Bibliotheque;
 use App\Entity\Livre;
-
+use App\Entity\Membre;
 
 class AppFixtures extends Fixture
 {
 
     /**
      * Generate data initialization for the livres : 
-     * [bibliotheque_description, titre, summary, number_of_page, date_parution]
+     * [bibliotheque_name, titre, summary, number_of_page, date_parution]
      * @return \\Generator
      */
     private static function livresDataGenerator()
     {
-        yield ["Description 1", "Livre 1", "Summary 1", 100, date_create("2020-01-01")];
-        yield ["Description 1", "Livre 2", "Summary 2", 200, date_create("2020-01-02")];
-        yield ["Description 1", "Livre 3", "Summary 3", 300, date_create("2020-01-03")];
-        yield ["Description 2", "Livre 4", "Summary 4", 400, date_create("2020-01-04")];
-        yield ["Description 2", "Livre 5", "Summary 5", 500, date_create("2020-01-05")];
-        yield ["Description 2", "Livre 6", "Summary 6", 600, date_create("2020-01-06")];
-        yield ["Description 2", "Livre 7", "Summary 7", 700, date_create("2020-01-07")];
-        yield ["Description 2", "Livre 8", "Summary 8", 800, date_create("2020-01-08")];
-        yield ["Description 2", "Livre 9", "Summary 9", 900, date_create("2020-01-09")];
-        yield ["Description 3", "Livre 10", "Summary 10", 1000, date_create("2020-01-10")];
+        yield ["La belle biblio", "Livre 1", "Summary 1", 100, date_create("2020-01-01")];
+        yield ["La belle biblio", "Livre 2", "Summary 2", 200, date_create("2020-01-02")];
+        yield ["La belle biblio", "Livre 3", "Summary 3", 300, date_create("2020-01-03")];
+        yield ["L'endoit des lecteurs", "Livre 4", "Summary 4", 400, date_create("2020-01-04")];
+        yield ["L'endoit des lecteurs", "Livre 5", "Summary 5", 500, date_create("2020-01-05")];
+        yield ["L'endoit des lecteurs", "Livre 6", "Summary 6", 600, date_create("2020-01-06")];
+        yield ["L'endoit des lecteurs", "Livre 7", "Summary 7", 700, date_create("2020-01-07")];
+        yield ["La belle biblio", "Livre 8", "Summary 8", 800, date_create("2020-01-08")];
+        yield ["La biblio de l'enfer", "Livre 99", "Summary 9", 666, date_create("2020-06-06")];
+        yield ["La biblio de l'enfer", "Livre 666", "Summary 10", 1000, date_create("2001-01-01")];
     }
 
     /**
      * Generate data initialization for bibiotheques :
-     * [description]
+     * [name, description]
      * @return \\Generator
      */
     private static function bibliothequesDataGenerator()
     {
-        yield ["Description 1"];
-        yield ["Description 2"];
-        yield ["Description 3"];
+        yield ["La belle biblio", "Dans cette bibliotheque, vous trouverez des livres avec beaucoup de visuels"];
+        yield ["La biblio de l'enfer", "Ici que des livres de merde"];
+        yield ["L'endoit des lecteurs", "Place aux livres, mais aussi aux lecteurs"];
+    }
+
+    /**
+     * Generate data for the memebers :
+     * [bibliotheque_name, name, description, birth_date]
+     * @return \\Generator
+     */
+    private static function membresDataGenerator()
+    {
+        yield ["La belle biblio", "Membre 1", "Description 1", date_create("1990-01-01")];
+        yield ["La biblio de l'enfer", "Membre 2", "Description 2", date_create("1990-01-02")];
+        yield ["L'endoit des lecteurs", "Membre 3", "Description 3", date_create("1990-01-03")];
     }
 
     public function load(ObjectManager $manager): void
     {
         $bibliothequeRepo = $manager->getRepository(Bibliotheque::class);
 
-        foreach (self::bibliothequesDataGenerator() as [$description]) {
+        foreach (self::bibliothequesDataGenerator() as [$name, $description]) {
             $bibliotheque = new Bibliotheque();
+            $bibliotheque->setName($name);
             $bibliotheque->setDescription($description);
             $manager->persist($bibliotheque);
         }
         $manager->flush();
 
-        foreach (self::livresDataGenerator() as [$bibliotheque_description, $titre, $summary, $number_of_page, $date_parution]) {
-            $bibliotheque = $bibliothequeRepo->findOneBy(["description" => $bibliotheque_description]);
+        foreach (self::livresDataGenerator() as [$bibliotheque_name, $titre, $summary, $number_of_page, $date_parution]) {
+            $bibliotheque = $bibliothequeRepo->findOneBy(["name" => $bibliotheque_name]);
             $livre = new Livre();
             $livre->setTitre($titre);
             $livre->setSummary($summary);
@@ -62,6 +75,17 @@ class AppFixtures extends Fixture
             $livre->setDateParution($date_parution);
             $bibliotheque->addLivre($livre);
             $manager->persist($bibliotheque);
+        }
+        $manager->flush();
+
+        foreach (self::membresDataGenerator() as [$bibliotheque_name, $name, $description, $birth_date]) {
+            $bibliotheque = $bibliothequeRepo->findOneBy(["name" => $bibliotheque_name]);
+            $membre = new Membre();
+            $membre->setName($name);
+            $membre->setDescription($description);
+            $membre->setBirthDate($birth_date);
+            $membre->setBibliotheque($bibliotheque);
+            $manager->persist($membre);
         }
         $manager->flush();
     }
