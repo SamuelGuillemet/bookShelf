dc018b91b2375489efd23903d7b8310e044fc97a
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-11-12 01:26:12 +0100

    Add form support to bibiliotheque #6

---
+++ src/Form/BibliothequeType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Bibliotheque;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BibliothequeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('membre', null, [
+                'disabled' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Bibliotheque::class,
+        ]);
+    }
+}
