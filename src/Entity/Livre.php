4707fe14c23aaef79846f8a45a76f473f16faeb8
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-11-12 01:16:42 +0100

    Modify Livre entity to add image upload and relations #2

--- src/Entity/Livre.php
+++ src/Entity/Livre.php
@@ -3,10 +3,15 @@
 namespace App\Entity;
 
 use App\Repository\LivreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=LivreRepository::class)
+ * @Vich\Uploadable
  */
 class Livre
 {
@@ -42,6 +47,46 @@ class Livre
      * @ORM\JoinColumn(nullable=false)
      */
     private $bibliotheque;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Auteur::class, inversedBy="livres")
+     */
+    private $auteur;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Vitrine::class, mappedBy="livres")
+     */
+    private $vitrines;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string
+     */
+    private $imageName;
+
+    /**
+     * @Vich\UploadableField(mapping="livres_img", fileNameProperty="imageName")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     *
+     * @var \DateTime
+     */
+    private $imageUpdatedAt;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Type::class, inversedBy="livres")
+     */
+    private $types;
+
+    public function __construct()
+    {
+        $this->vitrines = new ArrayCollection();
+        $this->types = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -110,6 +155,104 @@ class Livre
 
     public function __toString(): string
     {
-        return $this->titre;
+        return '[' . $this->id . '] ' . $this->titre;
+    }
+
+    public function getAuteur(): ?Auteur
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?Auteur $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Vitrine>
+     */
+    public function getVitrines(): Collection
+    {
+        return $this->vitrines;
+    }
+
+    public function addVitrine(Vitrine $vitrine): self
+    {
+        if (!$this->vitrines->contains($vitrine)) {
+            $this->vitrines[] = $vitrine;
+            $vitrine->addLivre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVitrine(Vitrine $vitrine): self
+    {
+        if ($this->vitrines->removeElement($vitrine)) {
+            $vitrine->removeLivre($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->imageUpdatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * @return Collection<int, Type>
+     */
+    public function getTypes(): Collection
+    {
+        return $this->types;
+    }
+
+    public function addType(Type $type): self
+    {
+        if (!$this->types->contains($type)) {
+            $this->types[] = $type;
+        }
+
+        return $this;
+    }
+
+    public function removeType(Type $type): self
+    {
+        $this->types->removeElement($type);
+
+        return $this;
     }
 }
